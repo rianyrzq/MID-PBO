9ffadc7b49696c065c5216c6893856d05b4c0c25
unknown <rianyrezeqyutamie@gmail.com>
2023-03-30 11:04:58 +0800

    MID SOURCE CODE

---
+++ TugasMid/TesErr0088.java
@@ -0,0 +1,15 @@
+//Riany Rezeqy Utamie 13020210088
+
+import java.util.Scanner;
+
+public class TesErr0088{
+   public static void main(String[] args){
+       Scanner input = new Scanner(System.in);
+       int i = input.nextInt();
+       int k = 100;
+       char c = 's';
+       boolean falses = true;
+       int j = i+1;
+       System.out.println("j adalah "+j+" k adalah "+k+" dan c adalah "+c+" dan true adalah "+falses);
+	}
+}
